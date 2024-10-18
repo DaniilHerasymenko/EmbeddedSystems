c7c6d4927c92cd84d5fdaf12f10a69c63d1609b9
Daniil <herasymenkodaniil@gmail.com>
2024-10-18 11:29:41 +0300

    Add files via upload

--- LAB2_PP-34_Dmytro_Vochakovskyi/Core/LCD/LCD.h
+++ LAB2_PP-34_Dmytro_Vochakovskyi/Core/LCD/LCD.h
@@ -14,5 +14,6 @@ void LCD_Init(void);
 void LCD_SendChar(char ch);
 void LCD_SendString(char *str, uint8_t size);
 void LCD_Clear(void);
+void lcd_move_to(uint8_t x, uint8_t y);
 
 #endif /* LCD_LCD_H_ */
